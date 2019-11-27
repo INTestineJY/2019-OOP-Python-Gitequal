20d1c767176167dd87b6b860fdf814d42351a44e
Jaeyeon Cho <patrick0249@sasa.hs.kr>
2019-11-28 00:26:07 +0900

    folium module tutorial update

---
+++ folium_JaeyeonCho.py
@@ -0,0 +1,21 @@
+import folium
+import os
+import numpy as np
+from folium import plugins
+import webbrowser
+
+print(folium.__version__)
+
+print("="*20)
+
+m = folium.Map([45, 3], zoom_start=4)
+
+folium.Marker([45.01, 3.01], popup='여긴 어디').add_to(m)
+
+m.save("map.html")
+
+html_file='map.html'
+
+webbrowser.open(html_file)
+
+folium.Map().get_root().render()
