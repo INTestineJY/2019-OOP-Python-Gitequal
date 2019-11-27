661ffe7d09776289acdbce400422a6b1c7724407
5252oioi <awesomedaisy@sasa.hs.kr>
2019-11-27 16:07:32 +0900

    Merge branch 'master' of https://github.com/INTestineJY/2019-OOP-Python-kadragon
    # Conflicts:
    #	README.md

--- soo young.py
+++ soo young.py
@@ -1,28 +1,32 @@
-'''
 import requests
 import json
 
 class WIFI :
-    def __init__(self, address):
+    def __init__(self):
+        pass
+
+    def juso_to_coor(self, address):
         self.address = address
         self.from_site = requests.get('http://apis.vworld.kr/new2coord.do?q='+address+'&apiKey=4BC8BA59-D75D-3BF2-AF93-A0730B4E148E&domain=http://map.vworld.kr/&output=json')
         self.coordinates = self.from_site.json()
         self.X = float(self.coordinates['EPSG_4326_X'])
         self.Y = float(self.coordinates['EPSG_4326_Y'])
-
+'''
 with open("wifidata.json", "r") as f:
     json_data = json.load(f)
     print(json.dumps(json_data))
+'''
 
+my_home = WIFI()
+my_school = WIFI()
 
-my_home = WIFI("서울특별시 노원구 노원로 62(공릉동, 효성화운트빌)")
-my_school = WIFI("세종특별자치시 달빛1로 265(아름동 산8)")
-
+my_home.juso_to_coor('서울특별시 노원구 노원로 62(공릉동, 효성화운트빌')
 print(my_home.X)
 print(my_home.Y)
 '''
 
 import json
 
-with open('wifidata.json', 'rb') as f:
+with open('wifidata.json', ) as f:
     root = json.load(f)
+'''
