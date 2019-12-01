3b5e0cea6e7f57c5f13fdaab02dee035c459a57b
안형주 <ahnhyungjoo@sasa.hs.kr>
2019-12-01 17:42:56 +0900

    README.md modify

--- Free_Wifi_Map.py
+++ Free_Wifi_Map.py
@@ -7,3 +7,25 @@ from PyQt5.QtCore import QCoreApplication
 
 with open('data.json') as data_file:
     data = json.load(data_file)
+
+pprint(data)
+#값 하나하나씩 접근
+data["map_dict"][0]['gangwon']
+data["map_dict"][0]['gyeonggi']
+data["map_dict"][0]['chungbuk']
+data["map_dict"][0]['chungnam']
+data["map_dict"][0]['jeonbuk']
+data["map_dict"][0]['gyeongbuk']
+data["map_dict"][0]['gyeongnam']
+data["map_dict"][0]['jeonnam']
+data["map_dict"][0]['seoul']
+data["map_dict"][0]['sejong']
+data["map_dict"][0]['busan']
+data["map_dict"][0]['ulsan']
+data["map_dict"][0]['incheon']
+data["map_dict"][0]['daejeon']
+data["map_dict"][0]['daegu']
+data["map_dict"][0]['gwangju']
+data["map_dict"][0]['jeju']
+
+
